bf4cb5117ada3b48be1ea309040b50b5ea334298
Boah Kim <boa8574.kim@gmail.com>
2024-02-23 22:09:14 -0500

    Toolkit.cpp

--- BKIM_Pr_#2_.cpp
+++ BKIM_Pr_#2_.cpp
@@ -1,5 +1,3 @@
-
-
 #include <iostream>
 #include <string>
 #include <iomanip>
@@ -13,10 +11,11 @@ int main()
     double Fahrenheit,Celsius;//temperature(it will transfer to integer later)
     double kilometer, mile,//distance(to get decimals)
     kilogram, pound;//weight
-    
-    cout<<"Eenter a country name: ";
+    char answer;
+  
+    cout<<"Enter a country name: ";
     getline(cin,country);//get a whole letters with a space.
-    
+  do{
     //display the menu
     cout<<"Converter Toolkit"<<endl;
     cout<<"---------------------"<<endl;
@@ -24,18 +23,18 @@ int main()
     cout<<"2.Distance Converter"<<endl;
     cout<<"3.Weight Converter"<<endl;
     cout<<"4.Quit"<<endl<<endl;
-    
+  
     //select a choice from the menu
     cout<<"Enter your choice(1-4): ";
     cin>>choice;//input the choice number
-    
-    //if statement for the choices
+
+  {//if statement for the choices
     if(choice==1)//temperature
     {cout<<"Please enter temperature in Celsius (such as 24): ";
         cin>>Celsius;
         Fahrenheit=Celsius*(9.0/5.0)+32;//for accuracy, it is better to calculate by double,first.
-        cout<<"It is "<<(int)Fahrenheit<<" in Fahrenheit."<<endl<<endl;}//change the double to integer.\
-    
+        cout<<"It is "<<(int)Fahrenheit<<" in Fahrenheit."<<endl<<endl;}//change the double to integer.
+
     else if(choice==2)//distance
     {cout<<"Please enter distance in kilometer: ";
         cin>>kilometer;
@@ -45,7 +44,7 @@ int main()
         {mile=kilometer*0.6;
         cout<<fixed<<setprecision(2)<<"It is "<<mile<<" miles"<<endl<<endl;}//to get two positions after decimal point.
     }
-    
+
     else if(choice==3)//weight
     {cout<<" please enter weight in Kilogram: ";
         cin>>kilogram;
@@ -55,14 +54,25 @@ int main()
         {pound=kilogram*2.2;
         cout<<fixed<<setprecision(1)<<"It is "<<pound<<" in pound."<<endl<<endl;}//to get one position after decimal point.
     }
-    
-    else//others
+    else if(choice==4)
+    {cout<<"Thank you for using the converter toolkit."<<endl<<endl;}
+  
+    else if(choice<1||choice>4)
     {cout<<"The input is invalid. "<<endl<<endl;}
-    
+  }
+    cout<<"Do you want to try again? (Y/N)"<<endl;
+    cin>>answer;
+  }while(answer == 'Y'||answer == 'y');
+       
+
+  
     cout<<country<<" sounds fun!!"<<endl<<endl<<endl;
-    
-    //display programmer's name,course,duedate
+
+    //display programmer's name
     cout<<"PROGRAMMER: Boa Kim"<<endl;
+
+
+}
 
   
 }
